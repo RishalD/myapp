07c6feb8b1da2b4599ecdc4593fcd7727c1a8c2a
Rishal <rishal.dhingan@gmail.com>
2022-03-06 10:21:42 -0600

    First Commit

---
+++ src/main/java/com/myapp/myapp/controller/MyRestController.java
@@ -0,0 +1,18 @@
+package com.myapp.myapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
+@RestController
+public class MyRestController {
+
+    @GetMapping("/")
+    public String myApp(){
+        return "Hello World! Time on server is " + LocalDate.now();
+    }
+
+
+}
